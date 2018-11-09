3d346dd6e051c5c761cff7bd97b6e2a5aec65c48
Jan <jcg@gmx.de>
2018-11-09 21:10:51 +0100

    Added a class in the domain for keeping consts. It is  not realy suited in the domain and therefore introduced a util package.

---
+++ src/main/java/de/tarent/challenge/util/Consts.java
@@ -0,0 +1,13 @@
+package de.tarent.challenge.util;
+
+/**
+ * This class containts throught the project. 
+ * Examples are: Names stored in string.
+ * Constant and ubiquously true statements.
+ * 
+ * The consts should be "basic" types like int,String,bool,...
+ * @author Jan
+ */
+public class Consts {
+    
+}
