864824ad5a27864b931fe71ddc8ffbb31fe4d24f
Jan <jcg@gmx.de>
2018-11-11 21:33:19 +0100

    added price to the productmodel

--- src/main/java/de/tarent/challenge/display/ProductModel.java
+++ src/main/java/de/tarent/challenge/display/ProductModel.java
@@ -4,24 +4,29 @@ import de.tarent.challenge.domain.IProduct;
 import de.tarent.challenge.domain.Product;
 
 import java.util.Set;
+import org.javamoney.moneta.Money;
 
 /**
- * This class implements the IProduct from the domain layer.
- * It's main purpase is to be used by the display layer.
- * Therefore it doubles as an ACL implementation.
- * 
+ * This class implements the IProduct from the domain layer. It's main purpase
+ * is to be used by the display layer. Therefore it doubles as an ACL
+ * implementation.
+ *
  * As the name suggests its a model in a MVC architecture.
+ *
  * @author Jan
  */
-public class ProductModel extends Product implements IProduct{
-    
-    public ProductModel(IProduct product)
-    {
+public class ProductModel extends Product implements IProduct {
+
+    public ProductModel(IProduct product) {
         super(product);
     }
 
     public ProductModel(String sku, String name, Set<String> eans) {
-        super(sku,name,eans);
-        
-    }    
+        super(sku, name, eans);
+    }
+
+    public ProductModel(String sku, String name, Set<String> eans, Money price) {
+        super(sku, name, eans, price);
+
+    }
 }
