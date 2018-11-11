643d8e7a5ad69bfb99f0d9bf1fa165d106b9d232
Jan <jcg@gmx.de>
2018-11-11 14:53:44 +0100

    Rewrote the factory usage and refactored it to be called Configuration according to springs termolagy. This is necessary because the IProductRepository is used as a autowire. Therefore every initalisiation of the perstitent Getters & Setters has to be done through spring. This as a whole eleminates the factories and proxy patterns reducing the application by keeping its configuribality, but underlining the dependincy on spring in the config package.

---
+++ src/main/java/de/tarent/challenge/config/ProductSetterConfiguration.java
@@ -0,0 +1,28 @@
+package de.tarent.challenge.config;
+
+import de.tarent.challenge.persistent.ProductStorer;
+import de.tarent.challenge.service.IProductSetter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * A Factory allowing to get the ProductServices between layers.
+ *
+ * @author Jan
+ */
+@Configuration
+@ComponentScan(value = {"de.tarent.challenge.persitent"})
+public class ProductSetterConfiguration {
+
+    /**
+     * Returns a IProductSetter with acces to a perstiant storage.
+     *
+     * @return
+     */
+    @Bean
+    public static IProductSetter getIProductSetter() {
+        return new ProductStorer();
+
+    }
+}
--- src/main/java/de/tarent/challenge/persistent/IProductRepository.java
+++ src/main/java/de/tarent/challenge/persistent/IProductRepository.java
@@ -2,8 +2,6 @@ package de.tarent.challenge.persistent;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,7 +13,6 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public interface IProductRepository extends JpaRepository<ProductDTO,String>{
 
-    
     public List<ProductDTO> findByName(String name);
     
     public List<ProductDTO> findBySku(String sku);
