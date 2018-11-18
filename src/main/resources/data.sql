7fcc5e0a0f39c72dc4ce55be950d4ea685d8e8ea
Jan <jcg@gmx.de>
2018-11-18 13:33:56 +0100

    updated the resources to have an available column and the integration tests now pass.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,11 @@
-INSERT INTO product (name, sku) VALUES ('Milch', '102');
-INSERT INTO product (name, sku) VALUES ('Brot', '2035');
-INSERT INTO product (name, sku) VALUES ('Käse', 'S-155');
-INSERT INTO product (name, sku) VALUES ('Wurst', '1488');
-INSERT INTO product (name, sku) VALUES ('Couscous', 'B001');
+INSERT INTO product (name, sku,available) VALUES ('Milch', '102',true);
+INSERT INTO product (name, sku,available) VALUES ('Brot', '2035',true);
+INSERT INTO product (name, sku,available) VALUES ('Käse', 'S-155',true);
+INSERT INTO product (name, sku,available) VALUES ('Wurst', '1488',true);
+INSERT INTO product (name, sku,available) VALUES ('Couscous', 'B001',true);
 
-INSERT INTO product_eans (PRODUCT_SKU , eans) VALUES ('102', '12345678');
 INSERT INTO product_eans (PRODUCT_SKU , eans) VALUES ('102', '77777777');
+INSERT INTO product_eans (PRODUCT_SKU , eans) VALUES ('102', '12345678');
 INSERT INTO product_eans (PRODUCT_SKU , eans) VALUES ('102', '23498128');
 INSERT INTO product_eans (PRODUCT_SKU , eans) VALUES ('2035', '34558821');
 INSERT INTO product_eans (PRODUCT_SKU , eans) VALUES ('2035', '12323410');
